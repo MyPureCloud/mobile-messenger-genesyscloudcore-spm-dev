079f38cee72aa23183bda5749d2dbdca0c3e5c7b
Andras Solyom <andras.solyom@genesys.com>
2025-05-20 11:14:40 +0200

    Add template

--- .template.Package.swift
+++ .template.Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudCore",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore_version_t1.14.0.rc15_commit_8ab25803577674e1c3cecd0d74c0e658597a5f45.zip",
-            checksum: "7013f7e5a7de03a48bf31754172eca1adf19fd383cda3deb431b792dc43c8a82"
+            url: "__source_url__",
+            checksum: "__checksum__"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudCore"
