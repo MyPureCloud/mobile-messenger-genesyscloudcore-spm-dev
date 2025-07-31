8edd3e6c52c513fd12ed13562b540be8f5b0bc84
PureCloud Jenkins <purecloud-jenkins@ininica.com>
2025-07-31 13:48:49 +0000

    [Fix] 1.15.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudCore",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore_version_t1.15.0.rc39_commit_a8847c837b7c905881af1ef7856fe80a3cdde5ff.zip",
-            checksum: "7523e2500a13130f8254d083bf075415867323d761122a983237f0fe0d5a52a5"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore_version_t1.15.0.rc40_commit_6072ab8d8f25175caf603109b6045bc850727e87.zip",
+            checksum: "61055946139d959eff14e5fca2dd15dcedc3fe282cbcf9cc745b419ab92dc055"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudCore"
