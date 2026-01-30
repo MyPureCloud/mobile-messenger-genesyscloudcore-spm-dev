a45ca5e2826130dedc464e5e976b697f25d63495
ec2-user <ec2-user@ip-10-25-184-100.ec2.internal>
2026-01-30 17:36:23 +0000

    [Update] 1.20.1-snapshot-feature-embed-sample-app-61

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudCore",
-            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore_version_1.20.1-snapshot-feature-embed-sample-app-60_commit_767875732f9b6a0076c6bd83129ec691f308c1f8.zip",
-            checksum: "35dcfb9b66b472310b21c4ed827657e86b704df296e9b5028e0bd0914ef15667"
+            url: "https://genesysdx.jfrog.io/artifactory/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore_version_1.20.1-snapshot-feature-embed-sample-app-61_commit_767875732f9b6a0076c6bd83129ec691f308c1f8.zip",
+            checksum: "e98e1ca08f28721c6003caf5f4a52a9e43a20069f893c1c0391de54b283d73a2"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudCore"
