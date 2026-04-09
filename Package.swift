11790d5951f5e4cff69ace01160d2ef8afd6c0df
ec2-user <ec2-user@ip-10-25-184-100.ec2.internal>
2026-04-09 17:59:54 +0000

    [Update] 0.2.1-SNAPSHOT-feature-gmms-15160-ios-pure-jfrog-7

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GenesysCloudCore",
-            url: "https://purecloud.jfrog.io/purecloud/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore-0.2.1-SNAPSHOT-feature-gmms-15160-ios-pure-jfrog-6.zip",
-            checksum: "013abf77080deca2211371ff1fb06757804817830d3b59b5a8257d475cf2912f"
+            url: "https://purecloud.jfrog.io/purecloud/genesys-cloud-ios.dev/GenesysCloudCore/GenesysCloudCore-0.2.1-SNAPSHOT-feature-gmms-15160-ios-pure-jfrog-7.zip",
+            checksum: "6c874ac82f5c53337e6f55e2908e253dec63f19b6874b9c9b365839decd53d0e"
         ),
         .testTarget(name: "PackageTests", dependencies: [
             "GenesysCloudCore"
